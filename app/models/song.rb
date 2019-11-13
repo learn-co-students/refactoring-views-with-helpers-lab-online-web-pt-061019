db4c488985de7f39e718983b237f93b062aebdd7
ZumbaIdol <david.a.foote@gmail.com>
2019-11-13 17:05:17 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+# Write the code for #artist_name and #artist_name= so that an Artist can be retrieved from, and associated with, a Song instance
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.find_or_create_by(:name => name)
   end
 end
