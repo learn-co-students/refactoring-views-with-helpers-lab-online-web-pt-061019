f1cfc31eba2135362a254daec5f29d7d740eb08f
Amber Taranto <ambzzz@gmail.com>
2020-01-08 16:36:51 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,12 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def artist_name
+    artist.name if artist
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
 end
